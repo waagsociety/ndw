87ebf0ee4ec7d92fc1daef8ac332911d8abe0959
Bert Spaan <bert.spaan@gmail.com>
2014-02-10 11:51:15 +0100

    index on mst direction

--- data/indexes.sql
+++ data/indexes.sql
@@ -2,6 +2,7 @@
 CREATE INDEX ON hectopunten USING btree(wvk_id);
 CREATE INDEX ON hectopunten USING btree(hectomtrng);
 CREATE INDEX ON mst USING btree(location);
+CREATE INDEX ON mst USING btree(direction);
 CREATE INDEX ON mst USING btree(id);
 CREATE INDEX ON tmcpoints USING btree(loc_nr);
 CREATE INDEX ON vild USING btree(loc_nr);
@@ -9,3 +10,4 @@ CREATE INDEX ON wegvakken USING btree(wvk_id);
 CREATE INDEX ON wegvakken USING btree(wegnummer);
 CREATE INDEX ON wegvakken USING btree(baansubsrt);
 CREATE INDEX ON wegvakken USING btree(rijrichtng);
+
