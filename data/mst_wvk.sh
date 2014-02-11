75f205389fddd823cc517cc16da846e451ddaf3d
Bert Spaan <bert.spaan@gmail.com>
2014-02-11 15:55:01 +0100

    script to create mst-nwb table

---
+++ data/mst_wvk.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#################################################################################
+# Create table with links between measurement sites and road segments
+#################################################################################
+
+psql -h localhost -U postgres -d ndw -f mst_wvk.sql
+
