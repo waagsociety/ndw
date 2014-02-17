47cc2b9d833637cfa77298340642d5ac4a916da1
Bert Spaan <bert.spaan@gmail.com>
2014-02-17 16:10:49 +0100

    code convention

--- src/find_wvk_for_mst.sql
+++ src/find_wvk_for_mst.sql
@@ -19,15 +19,15 @@ locTcm int;
 
 BEGIN
 	-- 0: haal het meetpunt record op, op basis van argument	
-	select gid, location, direction, distance as offset from mst where mst_id = _id  INTO meetpunt;
-	RAISE NOTICE 'gid: %, location: % direction: %, offset: %', meetpunt.gid, meetpunt.location, meetpunt.direction, meetpunt.offset;
+	SELECT location, direction, distance AS offset FROM mst WHERE mst_id = _id  INTO meetpunt;
+	RAISE NOTICE 'location: % direction: %, offset: %', meetpunt.location, meetpunt.direction, meetpunt.offset;
 	
 	-- 1. pak het VILD punt met alle relevante informatie
 	select loc_nr, loc_type, roadnumber, hstart_pos, hstart_neg from vild where loc_nr = meetpunt.location INTO vildlocatie;
 	RAISE NOTICE 'vild road before: %', vildlocatie.roadnumber;
 	
 	--1b. reformat the roadnumber so we can compare it with the wegvakken table
-    	select lpad(substring(vildlocatie.roadnumber from 2 for length(vildlocatie.roadnumber)), 3, '000') INTO wegId;
+  SELECT lpad(substring(vildlocatie.roadnumber from 2 for length(vildlocatie.roadnumber)), 3, '000') INTO wegId;
 	RAISE NOTICE 'vild road after: %', wegId;
 
 	--1c. reformat the direction so we can compare it with the wegvakken table
@@ -43,7 +43,7 @@ BEGIN
 	
 	--2. zoek het basis hectometerpaaltje, deze is afhankelijk van de richting van het meetpunt 
 	select wegvakken.wvk_id as wegvakid, wegnummer, hectomtrng, rijrichtng, afstand, beginkm from wegvakken JOIN hectopunten ON hectopunten.wvk_id = wegvakken.wvk_id where wegnummer = wegId AND hectomtrng = hLoc AND rijrichtng = wegDirection INTO paaltje;
-	RAISE NOTICE 'wegvakid paaltje: %', paaltje.wegvakid;
+	RAISE NOTICE 'wvk_id hectometerpaal: %', paaltje.wegvakid;
 
 	-- 3. bereken de plek van het meetpunt als de basishectometerpaal in meters en tel daar de offset van het meetpunt bij op
 	IF meetpunt.direction = 'negative' THEN
